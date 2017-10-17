4b43603a354c6ef90083819b427bdd499ede9a24
namwoo <garson1362@gmail.com>
2017-10-17 10:36:37 +0900

    use login and logout views from auth/ modify login template

--- instagram/config/urls.py
+++ instagram/config/urls.py
@@ -17,8 +17,9 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_view
 
-from member.views import signup, signin
+from member.views import signup
 from post.views import post_list, post_create, post_detail, comment_create
 
 urlpatterns = [
@@ -29,7 +30,8 @@ urlpatterns = [
     url(r'^post/(?P<post_pk>\d+)$', post_detail, name='post_detail'),
     url(r'^post/(?P<post_pk>\d+)/comment/$', comment_create, name='comment_create'),
     url(r'^member/signup/$', signup, name='signup'),
-    url(r'^member/signin/$', signin, name='signin'),
+    url(r'^member/login/$', auth_view.login, {'template_name': 'member/login.html'}, name='login'),
+    url(r'^member/logout/$', auth_view.logout, {'next_page':'login'}, name='logout'),
 ]
 urlpatterns += static(
     settings.MEDIA_URL,
--- instagram/member/views.py
+++ instagram/member/views.py
@@ -13,7 +13,7 @@ def signup(request):
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+            password = form.cleaned_data['password']
             email = form.cleaned_data['email']
 
             new_user = User.objects.create_user(first_name=first_name,
@@ -21,37 +21,16 @@ def signup(request):
                                                 username=username,
                                                 password=password,
                                                 email=email)
-            login(request, new_user)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
 
             return redirect('/post/')
     else:
         form = SignUpForm()
 
-        context = {
-            'form': form
-        }
+    context = {
+        'form': form
+    }
 
-        return render(request, 'member/signup.html', context)
-
-
-def signin(request):
-    if request.method == 'POST':
-        form = SignInForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/post/')
-        else:
-            return HttpResponse('로그인에 실패했습니다.')
-
-    else:
-        form = SignInForm()
-
-        context = {
-            'form': form
-        }
-
-    return render(request, 'member/signin.html', context)
+    return render(request, 'member/signup.html', context)
