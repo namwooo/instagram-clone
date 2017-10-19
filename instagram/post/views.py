64399035c7e0c7e005f80a1698e8e23d2b7ac45d
namwoo <garson1362@gmail.com>
2017-10-19 09:50:16 +0900

    comment on post views

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -23,29 +23,20 @@ def post_list(request):
     return render(request, 'post/post_list.html', context)
 
 
-# def post_create(request):
-#     if request.method == 'POST':
-#         form = PostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             post = Post.objects.create(photo=form.cleaned_data['photo'])
-#             return
-
-
 def post_create(request):
     """
-    :param request:
-    :return:
+    사용자로 부터 입력 받은 이미지 파일을 media/post에 저장하고,
+    그 파일 위치 정보를 데이터 베이스에 저장한다.
+    :param request: request to upload imagefile
+    :return: render to post_create.html
     """
     if request.method == 'POST':
         # POST 요청의 경우 PostForm인스턴스 생성과정에서 request.POST, request.FILES 사용
         form = PostForm(request.POST, request.FILES)
-        # form에 전달 된 데이터들이 Form field에 대해 유효 한지 검사
         if form.is_valid():
-            # 유효할 경우 Post 인스턴스 생성 및 저장
             post = Post.objects.create(photo=form.cleaned_data['photo'])
             return HttpResponse(f'<img src="{post.photo.url}">')
     else:
-        # GET 요청의 겨우 빈 form 인스턴스를 생성
         form = PostForm()
 
     context = {
@@ -57,34 +48,35 @@ def post_create(request):
 
 def post_detail(request, post_pk):
     """
-    1. 요청 받은 request와 pk로 해당 post 객체를 post에 할당
-    2.
-    :param request:
-    :return:
+    사용자가 원하는 하나의 포스트만을 보여준다.
+    :param request: request to display post details
+    :param post_pk: the post's primary key to access the post
+    :return: render to post_detail.html
     """
-    post = Post.objects.get(pk=post_pk)
-    post = get_object_or_404(Post, pk=post_pk)
+    post = get_object_or_404(Post, pk=post_pk)  # Post 객체가 없을 경우, 404 에러가 발생한다.
     comment_form = CommentForm()
+
     context = {
         'post': post,
         'comment_form': comment_form
-
     }
+
     return render(request, 'post/post_detail.html', context)
 
 
 def comment_create(request, post_pk):
     """
-
+    포스트에 댓글을 생성한다.
+    댓글 생성 후, 해당 포스트 댓글 맨 위를 사용자에게 보여준다. post_list.html에서
+    /post/#post-comments-{{ post.pk }}를 request로 가져와 next에 할당한다.
+    id가 post-comments-{{ post.pk }}인 html위치로 리다이렉트 한다.
     :param request:
+    :param post_pk:
     :return:
     """
-    # 'post_pk'에 해당하는 Post 인스턴스를  post에 할당
     post = get_object_or_404(Post, pk=post_pk)
     if request.method == 'POST':
-        # 데이터가 바인딩된 CommentForm 인스턴스를 form에 할당
         form = CommentForm(request.POST)
-        # 유효성 검사
         if form.is_valid():
             PostComment.objects.create(
                 post=post,
